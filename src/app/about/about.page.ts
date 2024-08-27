422021d399e6f0d5a6b3cd875eabb574e4b21980
sayada sudyindee <sayadasyd060650@gmail.com>
2024-08-27 12:57:09 +0700

    update

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage implements OnInit {
 
-  constructor() { }
+  constructor(private h:NavController) { }
 
   ngOnInit() {
   }
 
+  home(){
+    this.h.navigateRoot('/home');
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AboutPage } from './about/about.page';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private a:NavController) {}
+
+  aboutPage(){
+    this.a.navigateForward('/about');
+  }
 }
